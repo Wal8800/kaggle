7278c418009507f5a1a1ee0fc80f81171d5234bd
John <johnlaw8800@gmail.com>
2019-05-05 10:32:59 +1200

    Rework label to be in the process data

--- audio-tagging-2019/process_data.py
+++ audio-tagging-2019/process_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import dask.dataframe as dd
 import timeit
 import time
+import natural.date
 
 
 def generate_log_melspectrogram(fname):
@@ -16,24 +17,17 @@ def generate_log_melspectrogram(fname):
     return librosa.power_to_db(s, ref=np.max)
 
 
-def get_melspec_train_curated():
-    train_curated = pd.read_csv('data/train_curated.csv')
-    print(train_curated.head())
-    ddata = dd.from_pandas(train_curated, npartitions=3)
-
+def get_melspec_train_curated(dataframe, column_name):
+    ddata = dd.from_pandas(dataframe, npartitions=3)
     res = ddata.map_partitions(
-        lambda df: df.apply((lambda row: generated_padded_log_melspectrogram(row.fname, 2)), axis=1), meta=(None, 'f8')).compute(scheduler='threads')
+        lambda df: df.apply((lambda row: generated_padded_log_melspectrogram('data/train_curated/', row[column_name], 2)),
+                            axis=1),
+        meta=(None, 'f8')).compute(scheduler='threads')
 
-    print(res.shape)
     return res
 
 
-def generated_padded_log_melspectrogram(fname, duration):
-    try:
-        y, sr = librosa.load('data/train_curated/' + fname)
-    except Exception as error:
-        print(fname)
-        raise error
+def padd_y(y, duration, sr):
     audio_length = duration * sr
     if len(y) > audio_length:
         max_offset = len(y) - audio_length
@@ -46,30 +40,41 @@ def generated_padded_log_melspectrogram(fname, duration):
         else:
             offset = 0
         y = np.pad(y, (offset, audio_length - len(y) - offset), "constant")
+
+    return y
+
+
+def generated_padded_log_melspectrogram(directory, fname, duration):
+    try:
+        y, sr = librosa.load(directory + fname)
+    except Exception as error:
+        print(fname)
+        raise error
+
+    y = padd_y(y, duration, sr)
+
     s = librosa.feature.melspectrogram(y=y, sr=sr)
-    return librosa.power_to_db(s, ref=np.max)
+    return librosa.power_to_db(s)
 
 
-def get_and_save_melspec_train_curated_pickle():
-    start_time = time.time()
-    melspec = get_melspec_train_curated()
-    print(type(melspec))
-    print(time.time() - start_time)
-    melspec_df = pd.DataFrame({'mel_spectrogram': melspec})
-    # melspec_df.to_csv('train_curated_melspectrogram.csv', index=False)
+def padded_melspec_train_curated_pickle():
+    train_curated = pd.read_csv('data/train_curated.csv')
+    melspec = get_melspec_train_curated(train_curated, 'fname')
+
+    melspec_df = pd.DataFrame(
+        {
+            'mel_spectrogram': melspec,
+            'labels': train_curated['labels']
+        }
+    )
     melspec_df.to_pickle('padded_train_curated_melspectrogram.pickle')
 
 
 if __name__ == "__main__":
-    # get_and_save_melspec_train_curated_pickle()
+    start_time = time.time()
+    padded_melspec_train_curated_pickle()
+    print("Time taken: {}".format(natural.date.compress(time.time() - start_time)))
     melspec = pd.read_pickle('padded_train_curated_melspectrogram.pickle')
-    reshape_arr = []
-    print(melspec.shape)
-    print(melspec['mel_spectrogram'][0].shape)
-    for index, row in melspec.iterrows():
-        reshape_arr.append([
-            row["mel_spectrogram"]
-        ])
-    reshape_arr = np.array(reshape_arr)
-    print(reshape_arr.shape)
+    print(melspec.head())
+
 
--- audio-tagging-2019/train.py
+++ audio-tagging-2019/train.py
@@ -6,8 +6,6 @@ import time
 
 from model import simple_2d_conv
 from tensorflow._api.v1.keras.optimizers import SGD
-from tensorflow._api.v1.keras.callbacks import TensorBoard
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from natural import date
 
@@ -25,11 +23,11 @@ def calculate_overall_lwlrap_sklearn(truth, scores):
     return overall_lwlrap
 
 
-def reshape_dataframe_to_ndarray(df):
+def reshape_dataframe_to_ndarray(df, column_name):
     reshape_arr = []
     for index, row in df.iterrows():
         reshape_arr.append([
-            row["mel_spectrogram"]
+            row[column_name]
         ])
     return np.array(reshape_arr)
 
@@ -40,16 +38,16 @@ def train():
     config.gpu_options.allow_growth = True
     tf.keras.backend.set_session(tf.Session(config=config))
 
-    train_curated = pd.read_csv('data/train_curated.csv')
-    labels = train_curated['labels'].str.get_dummies(sep=',')
     train_curated_melspec = pd.read_pickle('padded_train_curated_melspectrogram.pickle')
+    labels = train_curated_melspec['labels'].str.get_dummies(sep=',')
 
     # Assume the specotrgram will be the same size after processing the data
     max_row = train_curated_melspec['mel_spectrogram'][0].shape[0]
     max_col = train_curated_melspec['mel_spectrogram'][0].shape[1]
 
     print('Max row size: ', max_row, 'Max column row size', max_col)
-    melspec_ndarray = reshape_dataframe_to_ndarray(train_curated_melspec).reshape((labels.shape[0], max_row, max_col, 1))
+    melspec_ndarray = reshape_dataframe_to_ndarray(train_curated_melspec, "mel_spectrogram").reshape(
+        (labels.shape[0], max_row, max_col, 1))
 
     kf = KFold(n_splits=5, shuffle=True)
     fold_scores = []
@@ -66,27 +64,25 @@ def train():
         model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
         callbacks = [
-            TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False,
-                        write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None,
-                        embeddings_data=None, update_freq='epoch')
+            tf.keras.callbacks.TensorBoard(log_dir='./logs/fold_{}'.format(current_fold), histogram_freq=0,
+                                           batch_size=32, write_graph=True, write_grads=False, write_images=False,
+                                           embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None,
+                                           embeddings_data=None, update_freq='epoch'),
+            tf.keras.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=10),
+            # tf.keras.callbacks.ModelCheckpoint('./models/best_{}.h5'.format(current_fold),
+            #                                    monitor='val_loss', verbose=1, save_best_only=True)
         ]
 
-        model.fit(x_train, y_train, batch_size=32, epochs=10, callbacks=callbacks)
+        model.fit(x_train, y_train, batch_size=32, epochs=10, callbacks=callbacks, validation_split=0.2)
         y_pred = model.predict(x_test)
 
         lwlrap = calculate_overall_lwlrap_sklearn(y_test, y_pred)
         print("Fold {} Score: {}".format(current_fold, lwlrap))
         current_fold += 1
         fold_scores.append(lwlrap)
-        # model_json = model.to_json()
-        # with open("./models/model.json", "w") as json_file:
-        #     json_file.write(model_json)
-        #
-        # model.save_weights("./models/model.h5")
     print("Average Fold Score:", np.mean(fold_scores))
-    print("Time taken: {}".format(date.compress(time.time()-start_time)))
+    print("Time taken: {}".format(date.compress(time.time() - start_time)))
 
 
 if __name__ == "__main__":
     train()
-
